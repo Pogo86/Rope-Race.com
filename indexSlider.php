d61489917c5975ca718422e00de5642069de9bb5
Rich Ellinson <rich@notebookds.com>
2017-12-31 14:54:52 +0000

    Get Fit 2018 1.1

--- indexSlider.php
+++ indexSlider.php
@@ -4,6 +4,7 @@
 			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
 			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
 		</ol>
 		<div class="carousel-inner" role="listbox">
 			<!-- Slide One - Set the background image for this slide in the line below -->
@@ -27,6 +28,13 @@
 					<p></p>
 				</div>
 			</a>
+			<!-- Slide Three - Set the background image for this slide in the line below -->
+			<a href="#" class="carousel-item" style="background-image: url(assets/img/sliders/homeSlide_fit2018.jpg)">
+				<div class="carousel-caption d-none d-md-block">
+					<h3></h3>
+					<p></p>
+				</div>
+			</a>		
 		</div>
 		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
