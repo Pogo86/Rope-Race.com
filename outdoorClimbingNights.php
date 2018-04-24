8ae8a44451abb98df7bb47326fa87c152fe87bfb
Rich Ellinson <rich@notebookds.com>
2018-04-24 15:04:17 +0100

    Revert "24th April"
    24th update 1.2

--- outdoorClimbingNights.php
+++ outdoorClimbingNights.php
@@ -28,11 +28,7 @@
             <a><img class="card-img-top" src="assets/img/windgatherRocks700x400.jpg" alt="Windgather Rocks - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
-				  <a>Windgather Rocks<small><br>Tuesday 24<sup>th</sup> April</a>
-=======
-                <a>Windgather Rocks<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
+				  <a>Windgather Rocks<small><br>Tuesday 24<sup>th</sup> April</small></a>
               </h4>
               <p class="card-text">A beginner-friendly crag amply provided with square-cut jugs and picnic spots. Good quality, well travelled gritstone. Generally easy, slabby climbs popular with beginners.</p>
               <a href="https://www.google.com/maps/place/53%C2%B018'06.0%22N+2%C2%B000'21.0%22W/@53.30167,-2.0080187,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.30167!4d-2.00583">Map Link</a>
@@ -44,11 +40,7 @@
             <a><img class="card-img-top" src="assets/img/hobsonMoorQuarry700x400.jpg" alt="Hobson Moor Quarry - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Hobson Moor Quarry<small><br>Tuesday 1<sup>st</sup> May</small></a>
-=======
-                <a>Hobson Moor Quarry<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">A pleasant (for a quarry!) climbing ground with easy access and good 'crack' with the locals. Mostly good rock on the right side of vertical! Routes mainly VS and above approx 15 - 20m long.</p>
               <a href="https://www.google.com/maps/place/53%C2%B028'01.0%22N+2%C2%B001'05.0%22W/@53.46695,-2.0202487,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.46695!4d-2.01806">Map Link</a>
@@ -60,11 +52,7 @@
             <a><img class="card-img-top" src="assets/img/castleNaze700x400.jpg" alt="Castle Naze - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Castle Naze<small><br>Tuesday 8<sup>th</sup> May</small></a>
-=======
-                <a>Castle Naze<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">A minor edge of mostly sound, weathered natural rock, with a looser sandy section beyond. Exposed to the elements, but quick drying with the majority of routes in the low to middle grades up to 15m in height.</p>
               <a href="https://www.google.com/maps/place/53%C2%B018'09.0%22N+1%C2%B055'24.0%22W/@53.3025,-1.9255187,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.3025!4d-1.92333">Map Link</a>
@@ -77,11 +65,7 @@
             <a><img class="card-img-top" src="assets/img/burbageNorth700x400.jpg" alt="Burbage North - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Burbage North<small><br>Tuesday 15<sup>th</sup> May</small></a>
-=======
-                <a>Burbage North<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">A fairly continuous section of small natural buttress (8 - 15m) of good grit. A popular Summer evening soloing and bouldering venue with routes mainly in the lower to middle grades.</p>
               <a href="https://www.google.com/maps/place/53%C2%B020'35.2%22N+1%C2%B036'37.8%22W/@53.3431,-1.6126887,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.3431!4d-1.6105">Map Link</a>
@@ -94,11 +78,7 @@
             <a><img class="card-img-top" src="assets/img/yarncliffeQuarry700x400.jpg" alt="Yarncliffe Quarry - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Yarncliffe Quarry<small><br>Tuesday 22<sup>nd</sup> May</small></a>
-=======
-                <a>Yarncliffe Quarry<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">The quarry and nearby edge are named after the millstones that used to be quarried here; an old name for them was querns and the corruption 'yarn'-cliffe (and also 'Wharn'-cliffe) indicates a location of this ancient industry.</p>
              <a href="https://www.google.com/maps/place/53%C2%B018'40.1%22N+1%C2%B037'06.1%22W/@53.31113,-1.6205587,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.31113!4d-1.61837">Map Link</a>
@@ -111,11 +91,7 @@
             <a><img class="card-img-top" src="assets/img/stanageEdge700x400.jpg" alt="Stanage Edge - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Stanage Edge Popular<small><br>Tuesday 29<sup>th</sup> May</small></a>
-=======
-                <a>Stanage Edge Popular<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">The highest and most continuous section of the edge, some of the longest routes on natural grit (up to 23m).</p>
               <a href="https://www.google.com/maps/place/53%C2%B020'34.1%22N+1%C2%B038'02.8%22W/@53.3428,-1.6362887,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.3428!4d-1.6341">Map Link</a>
@@ -128,11 +104,7 @@
             <a><img class="card-img-top" src="assets/img/burbageSouth700x400.jpg" alt="Burbage South - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Burbage South Valley Boulders<small><br>Tuesday 5<sup>th</sup> June</small></a>
-=======
-                <a>Burbage South Valley Boulders<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">The valley boulders at Burbage offer some of the most friendly bouldering around. The landings are excellent and nothing gets too high.</p>
               <a href="https://www.google.com/maps/place/53%C2%B019'17.0%22N+1%C2%B036'24.5%22W/@53.3214,-1.6089887,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.3214!4d-1.6068">Map Link</a>
@@ -145,11 +117,7 @@
             <a><img class="card-img-top" src="assets/img/lawrencefield700x400.jpg" alt="Lawrencefield - Rope Race Climbing"></a>
             <div class="card-body">
               <h4 class="card-title">
-<<<<<<< HEAD
                 <a>Lawrencefield<small><br>Tuesday 12<sup>th</sup> June</small></a>
-=======
-                <a>Lawrencefield<small><br>Date to be announced</small></a>
->>>>>>> parent of 7d03767... 27th March Outdoor Update
               </h4>
               <p class="card-text">A few quarried bays and a smaller edge. Rock generally sound. Main bay is sheltered from prevailing winds and has a magnificent back wall (22m high).</p>
               <a href="https://www.google.com/maps/place/53%C2%B019'01.2%22N+1%C2%B037'25.0%22W/@53.317,-1.6257887,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d53.317!4d-1.6236">Map Link</a>
