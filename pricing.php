ee3bdd24d5b65fb55ded15f2a0117b13e369540c
Rich Ellinson <rich@notebookds.com>
2018-05-06 12:12:39 +0100

    6thMay pricing Update 1.2

--- pricing.php
+++ pricing.php
@@ -22,8 +22,84 @@
 
 		<div class="row">
 			<div class="col-lg-12">
-				<h3>About Us</h3>
-				
+				<div id="accordion">
+				  <div class="card">
+					<div class="card-header" id="headingOne">
+					  <h5 class="mb-0">
+						<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+						  Membership &amp; Passes
+						</button>
+					  </h5>
+					</div>
+
+					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+					  <div class="card-body">
+						<div class="row">
+							<!-- img goes here -->
+							<div class="col-md-6 col-sm-12">
+								<img class="img-fluid" src="http://via.placeholder.com/350x350" alt="">
+							</div>
+							<!-- Prices go here -->
+							<div class="col-md-6 col-sm-12">
+								<table class="table table-hover">
+								  <thead>
+									<tr>
+									  <th scope="col">First</th>
+									  <th scope="col">Last</th>
+									  <th scope="col">Handle</th>
+									</tr>
+								  </thead>
+								  <tbody>
+									<tr>
+									  <td>Mark</td>
+									  <td>Otto</td>
+									  <td>@mdo</td>
+									</tr>
+									<tr>
+									  <td>Jacob</td>
+									  <td>Thornton</td>
+									  <td>@fat</td>
+									</tr>
+									<tr>
+									  <td colspan="2">Larry the Bird</td>
+									  <td>@twitter</td>
+									</tr>
+								  </tbody>
+								</table>							
+							</div>
+						</div>
+					  </div>
+					</div>
+				  </div>
+				  <div class="card">
+					<div class="card-header" id="headingTwo">
+					  <h5 class="mb-0">
+						<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+						  Day Rates
+						</button>
+					  </h5>
+					</div>
+					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+					  <div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					  </div>
+					</div>
+				  </div>
+				  <div class="card">
+					<div class="card-header" id="headingThree">
+					  <h5 class="mb-0">
+						<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+						  Collapsible Group Item #3
+						</button>
+					  </h5>
+					</div>
+					<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+					  <div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					  </div>
+					</div>
+				  </div>
+				</div>
 			</div>
 		</div>
 	</div>
